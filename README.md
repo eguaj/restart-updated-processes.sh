9d5e080f133d02c9b7b410bad8c09739e41540f1
Jérôme Augé <jerome.auge@anakeen.com>
2015-03-03 09:18:39 +0100

    Public release

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
 # restart-updated-processes.sh
-Restart (upgrade|upgrade)d processes
+
+Restart (update|upgrade)d processes.
+
+After updating a system library (e.g. libc, libssl or libcurl), the processes
+loaded on boot will still use the old library which was also loaded on boot.
+
+This script is meant to detect theses processes (libs marked as deleted in
+`/proc/%pid/maps`) and restart them in order to load the new library.
+
+The restart is done by matching the processes name (or command line) against a
+list of known names with the corresponding restart instructions.
+
+At the present time, this script is meant for use with Ubuntu 12.04 LTS and
+Ubuntu 14.04 LTS.
+
+# Usage
+
+Default mode is to scan the processes and report what would be done:
+
+    # ./restart-updated-processes.sh
+    [...]
+    18881   /usr/lib/postgresql/9.1/bin/postgres [/usr/lib/postgresql/9.1/bin/postgres -D /var/lib/postgresql/9.1/main -c config_file=/etc/postgresql/9.1/main/postgresql.conf ]
+            # service postgresql restart
+    32397   /usr/sbin/sshd [/usr/sbin/sshd -D ]
+            # service ssh restart
+    514 /sbin/dhclient3 [dhclient3 -e IF_METRIC=100 -pf /var/run/dhclient.eth0.pid -lf /var/lib/dhcp/dhclient.eth0.leases -1 eth0 ]
+            # ifdown eth0
+            # ifup   eth0
+    529 /usr/sbin/rsyslogd [rsyslogd -c5 ]
+            # service rsyslog restart
+    626 /sbin/getty [/sbin/getty -8 38400 tty4 ]
+            # kill 626
+    [...]
+
+To effectively restart the processes, use the `restart` argument:
+
+    # ./restart-updated-processes.sh restart
+    [...]
+    18881   /usr/lib/postgresql/9.1/bin/postgres [/usr/lib/postgresql/9.1/bin/postgres -D /var/lib/postgresql/9.1/main -c config_file=/etc/postgresql/9.1/main/postgresql.conf ]
+    restart postgresql
+    * Restarting PostgreSQL 9.1 database server
+    ...done.
+    32397   /usr/sbin/sshd [/usr/sbin/sshd -D ]
+    restart ssh
+    ssh stop/waiting
+    ssh start/running, process 2215
+    514 /sbin/dhclient3 [dhclient3 -e IF_METRIC=100 -pf /var/run/dhclient.eth0.pid -lf /var/lib/dhcp/dhclient.eth0.leases -1 eth0 ]
+    ssh stop/waiting
+    ssh start/running, process 2372
+    529 /usr/sbin/rsyslogd [rsyslogd -c5 ]
+    restart rsyslog
+    rsyslog stop/waiting
+    rsyslog start/running, process 2404
+    626 /sbin/getty [/sbin/getty -8 38400 tty4 ]
+    [...]
+
+The script can also be executed "on-the-fly" on a remote machine without first
+copying it locally:
+
+    $ cat restart-updated-processes.sh | ssh root@server.example.net /bin/bash /dev/stdin
+    
+    $ cat restart-updated-processes.sh | ssh root@server.example.net /bin/bash /dev/stdin restart
+
---
+++ restart-updated-processes.sh
@@ -0,0 +1,277 @@
+#!/bin/bash
+
+shopt -s nullglob
+
+function check_compatibility {
+    local DISTRIB=$(lsb_release -s -i)-$(lsb_release -s -r)
+    case $DISTRIB in
+	Ubuntu-12.04*)
+	    ;;
+	Ubuntu-14.04*)
+	    ;;
+	*)
+	    printf "Error: unsupported distribution '%s'.\n" "$DISTRIB"
+	    exit 1
+    esac
+}
+
+function echo_service {
+    local NAME=$1
+    local OP=$2
+    if [ -z "$OP" ]; then
+	OP="restart"
+    fi
+    if [ "$COMMAND" = "restart" ]; then
+	echo "$OP" "$NAME"
+	service "$NAME" "$OP"
+    else
+	printf "\t# service %s %s\n" "$NAME" "$OP"
+    fi
+}
+
+function echo_kill {
+    local PID=$1
+    if [ "$COMMAND" = "restart" ]; then
+	kill "$PID"
+    else
+	printf "\t# kill %s\n" "$PID"
+    fi
+}
+
+function echo_run {
+    local CMD=$1
+    shift
+    if [ "$COMMAND" = "restart" ]; then
+	"$CMD" "$@" &
+    else
+	local ARGS=""
+	while [ $# -gt 0 ]; do
+	    ARGS=$(printf "%s %q" "$ARGS" "$1")
+	    shift
+	done
+	printf "\t# %s %s\n" "$CMD" "$ARGS"
+    fi
+}
+
+function echo_intf_restart {
+    local CMDLINE=$1
+    local INTF=$(echo "$CMDLINE" | awk '{print $(NF)}')
+    if [ -z "$INTF" ]; then
+	printf "\t\t*** dhclient interface not found ***\n"
+	return
+    fi
+    if [ "$COMMAND" = "restart" ]; then
+	ifdown "$INTF"
+	ifup "$INTF"
+    else
+	printf "\t# ifdown %s\n" "$INTF"
+	printf "\t# ifup   %s\n" "$INTF"
+    fi
+}
+
+function profile_restart {
+    local PID=$1
+    local BIN=$2
+    local CMDLINE=$3
+
+    kill -0 "$PID"
+    if [ $? -ne 0 ]; then
+	return
+    fi
+
+    printf "%s\t%s [%s]\n" "$PID" "$BIN" "$CMDLINE"
+
+    case "$CMDLINE" in
+	*/BackupPC\ *)
+	    echo_service backuppc restart
+	    return
+	    ;;
+	*/qwebirc/run.py*)
+	    echo_service qwebirc stop
+	    echo_service qwebirc start
+	    return
+	    ;;
+	*/bin/te_request_server*)
+	    echo_service ted restart
+	    return
+	    ;;
+	*/usr/sbin/xe-daemon*)
+	    echo_service xe-linux-distribution restart
+	    return
+	    ;;
+    esac
+
+    case "$BIN" in
+	/sbin/mdadm)
+	    echo_service mdadm restart
+	    ;;
+	/usr/sbin/irqbalance)
+	    echo_service irqbalance restart
+	    ;;
+	/usr/lib/ipsec/pluto)
+	    echo_service ipsec restart
+	    ;;
+	/usr/sbin/dnsmasq)
+	    echo_service dnsmasq restart
+	    ;;
+	/usr/sbin/pcscd)
+	    echo_service pcscd restart
+	    ;;
+	/usr/lib/postfix/master)
+	    echo_service postfix restart
+	    ;;
+	/usr/sbin/acpid)
+	    echo_service acpid restart
+	    ;;
+	/usr/sbin/apache2)
+	    echo_service apache2 restart
+	    ;;
+	/usr/sbin/xinetd)
+	    echo_service xinetd restart
+	    ;;
+	/usr/sbin/inetd)
+	    echo_service openbsd-inetd restart
+	    ;;
+	postgres:|*postgresql*)
+	    echo_service postgresql restart
+	    ;;
+	/usr/sbin/zabbix_agentd)
+	    echo_service zabbix-agent restart
+	    ;;
+	/usr/sbin/zabbix_server)
+	    echo_service zabbix-server restart
+	    ;;
+	/usr/sbin/atd)
+	    echo_service atd restart
+	    ;;
+	/usr/sbin/sshd)
+	    echo_service ssh restart
+	    ;;
+	/usr/bin/redis-server*)
+	    echo_service redis-server restart
+	    ;;
+	*/gitlab/*)
+	    echo_service gitlab restart
+	    ;;
+	/usr/sbin/named)
+	    echo_service bind9 restart
+	    ;;
+	/usr/sbin/rsyslogd)
+	    echo_service rsyslog restart
+	    ;;
+	/usr/sbin/syslog-ng)
+	    echo_service syslog-ng  restart
+	    ;;
+	/usr/sbin/mysqld)
+	    echo_service mysql restart
+	    ;;
+	/usr/sbin/ntpd)
+	    echo_service ntp restart
+	    ;;
+	/usr/sbin/cron)
+	    echo_service cron restart
+	    ;;
+	/usr/sbin/openvpn)
+	    echo_service openvpn restart
+	    ;;
+	/usr/sbin/dhcpd)
+	    echo_service isc-dhcp-server restart
+	    ;;
+	proftpd:)
+	    echo_service proftpd restart
+	    ;;
+	/sbin/rpcbind)
+	    echo_service portmap restart
+	    ;;
+	/usr/sbin/rpc.idmapd)
+	    echo_service idmapd restart
+	    ;;
+	/sbin/rpc.statd)
+	    echo_service statd restart
+	    ;;
+	/usr/sbin/rpc.mountd)
+	    echo_service nfs-kernel-server restart
+	    ;;
+	/usr/sbin/ircd-hybrid)
+	    echo_service ircd-hybrid restart
+	    ;;
+	/sbin/udevd|/lib/systemd/systemd-udevd)
+	    echo_service udev restart
+	    ;;
+	/bin/dbus-daemon)
+	    echo_service dbus restart
+	    ;;
+	/usr/bin/memcached)
+	    echo_service memcached restart
+	    ;;
+	/usr/sbin/lpd)
+	    echo_service lpd restart
+	    ;;
+	avahi-daemon:*)
+	    echo_service avahi-daemon restart
+	    ;;
+	nginx:*)
+	    echo_service nginx restart
+	    ;;
+	php-fpm:*)
+	    echo_service php5-fpm restart
+	    ;;
+	/usr/sbin/VBoxService)
+	    echo_service virtualbox-guest-utils restart
+	    ;;
+	/usr/sbin/nullmailer-send)
+	    echo_service nullmailer restart
+	    ;;
+	/usr/sbin/vmtoolsd)
+	    echo_kill "$PID"
+	    echo_run /usr/sbin/vmtoolsd
+	    ;;
+	/sbin/upstart-socket-bridge)
+	    echo_kill "$PID"
+	    ;;
+	/sbin/getty)
+	    echo_kill "$PID"
+	    ;;
+	/sbin/dhclient|/sbin/dhclient3)
+	    echo_intf_restart "$CMDLINE"
+	    ;;
+	/sbin/init)
+	    ;;
+	sshd:)
+	    ;;
+	"")
+	    ;;
+	*)
+	    printf "\t\t*** unknown ***\n"
+	    ;;
+    esac
+}
+
+function main {
+    check_compatibility
+    COMMAND=$1
+    declare -A DONE
+    for CMDLINE in /proc/*/cmdline; do
+	PDIR=$(dirname "$CMDLINE")
+	( grep '(deleted)' "$PDIR/maps" | grep '\.so' ) > /dev/null 2>&1
+	if [ $? -ne 0 ]; then
+	    continue
+	fi
+	PID=$(basename "$PDIR")
+	if [ "$PID" = "self" ]; then
+	    continue
+	fi
+	BIN=$(perl -ne 'BEGIN{$/=undef};@_=split("\x00", $_);printf("%s\n",$_[0]);' "$CMDLINE")
+	CMDLINE=$(sed -e 's/\x0/ /g' "$CMDLINE")
+	if [ "${BIN:0:1}" != "/" ]; then
+	    FQBIN=$(which -- "$BIN")
+	    if [ -n "$FQBIN" ]; then
+		BIN=$FQBIN
+	    fi
+	fi
+	profile_restart "$PID" "$BIN" "$CMDLINE"
+    done
+}
+
+main "$@"
+
